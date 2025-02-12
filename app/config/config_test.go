6c3e27bf80df5bf635f054a29b207ae756defdbb
meesooqa <meesooqa@hotmail.com>
2025-02-12 10:07:31 +0300

    change config test

--- app/config/config_test.go
+++ app/config/config_test.go
@@ -19,7 +19,7 @@ func TestLoad(t *testing.T) {
 	assert.Equal(t, "to1", r.Channels["name1"].To)
 
 	assert.Equal(t, "⚡️", r.Channels["name1"].Filters.Bolt.Prefix)
-	assert.Equal(t, "❗️", r.Channels["name1"].Filters.Important.Prefix)
+	assert.Empty(t, r.Channels["name1"].Filters.Important)
 
 	assert.Equal(t, "from2", r.Channels["name2"].From)
 	assert.Equal(t, "to2", r.Channels["name2"].To)
